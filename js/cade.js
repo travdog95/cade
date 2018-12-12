41c9e207cf04690902c57fac178d36d1af64dd43
travdog95 <travis@tkopeters.com>
2018-12-12 16:41:56 -0700

    Testing

---
+++ js/cade.js
@@ -0,0 +1,14 @@
+let cade = () => {
+  var messages = [
+    "Never gonna give you up",
+    "Never gonna let you down",
+    "Never gonna run around",
+    "And desert you!"
+  ];
+
+  for (let i = 0; i < messages.length; i++) {
+    console.log(messages[i]);
+  }
+};
+
+cade();
